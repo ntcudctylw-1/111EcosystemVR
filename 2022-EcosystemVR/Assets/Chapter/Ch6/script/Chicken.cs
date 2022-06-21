9ec7ceeb1753c330ce3fa6627293e8b0df10850a
LJT1109 <105228706+LJT1109@users.noreply.github.com>
2022-06-22 04:44:04 +0800

    20220622-start

--- 2022-EcosystemVR/Assets/Chapter/Ch6/script/Chicken.cs
+++ 2022-EcosystemVR/Assets/Chapter/Ch6/script/Chicken.cs
@@ -1,33 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Chicken : MonoBehaviour
 {
-    public GameObject chicken;
-    public float Radius = 3;
-    public float lerptime = 0.01f;
-    public float move_dis = 1f;
-    Vector3 target;
+    public Animator Animator;
+
     private void Start()
     {
-        chicken = this.transform.GetChild(0).gameObject;
-        target = Vector3.zero;
         
-        InvokeRepeating("Chicken_move", 1, 3);
     }
+    bool walk = false;
     private void Update()
     {
-        chicken.transform.LookAt(target);
-        chicken.transform.position = Vector3.Lerp(chicken.transform.position, this.transform.position + target, lerptime);
+        Vector3 v = GetComponent<NavMeshAgent>().velocity;
+        float temp = Mathf.Pow((v.x * v.x + v.z * v.z), 1f / 2f);
+        if(temp > 0.5 && !walk)
+        {
+            walk = true;
+            Animator.SetTrigger("walk");
+        }
+        if(temp < 1)
+        {
+            walk = false;
+        }
+
+        //  Debug.Log(Mathf.Pow((v.x * v.x + v.z * v.z), 1f / 2f));
+        Animator.SetFloat("v",temp );
     }
 
-
-    void Chicken_move()
-    {
-        target = new Vector3(Random.Range(-Radius, Radius), 0, Random.Range(-Radius, Radius));
-        Debug.Log(target);
-        move_dis = Random.Range(-move_dis, move_dis);
-    }
-    
 }
